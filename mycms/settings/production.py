499b0bcb5194b6aa07fb742db040e7e8bd3872bf
Neerav Panchal <masterpanchieneerav@gmail.com>
2022-07-13 19:43:15 +0530

    [13-07-22] - Add host.

--- mycms/settings/production.py
+++ mycms/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['https://neeravpanchal26.pythonanywhere.com/']
+
+ROOT_CONF = "mycms.urls"
+
+SECRET_KEY = ')iij*sov_i7sp0yp*=_@ngu1n@szcbc79^2^#8$53-i7#(lc!u'
+
 try:
     from .local import *
 except ImportError:
